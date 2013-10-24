70a5a8866b1b66361c6641817f3207c0267c593a
Neamar <neamar@neamar.fr>
2013-10-24 13:44:19 +0200

    Split datas and metadatas

--- test/pdf-results.js
+++ test/pdf-results.js
@@ -8,18 +8,18 @@ var pdf = require('../lib/hydrater-pdf');
 describe('Test pdf results', function() {
   it('returns the correct informations', function(done) {
     var document = {
-      metadatas: {}
+      datas: {}
     };
 
     pdf(__dirname + "/samples/cv.pdf", document, function(err, document) {
       if(err) {
         throw err;
       }
-      document.should.have.property('metadatas');
+      document.should.have.property('datas');
       document.should.have.property('binary_document_type', "document");
-      document.metadatas.should.have.property('html');
+      document.datas.should.have.property('html');
 
-      document.metadatas.html
+      document.datas.html
         .should.include('Matt<span class="_ _0"></span>h<span class="_ _1"></span>i<span class="_ _0"></span>e<span class="_ _2"></span>u');
       done();
     });
