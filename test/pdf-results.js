8dc8f246c00d45125f0a4576e382433ce17cf103
Hugo Duroux <hugoduroux@free.fr>
2014-01-21 16:08:44 +0100

    first commit

--- test/pdf-results.js
+++ test/pdf-results.js
@@ -3,7 +3,7 @@
 require('should');
 
 var pdf = require('../lib/');
-
+var fs = require('fs');
 
 describe('Test pdf results', function() {
   it('returns the correct informations', function(done) {
@@ -21,6 +21,24 @@ describe('Test pdf results', function() {
 
       document.datas.html
         .should.include('Matt<span class="_ _0"></span>h<span class="_ _1"></span>i<span class="_ _0"></span>e<span class="_ _2"></span>u');
+      done();
+    });
+  });
+
+  it.only('returns the correct informationsazzer', function(done) {
+    var document = {
+      datas: {}
+    };
+
+    pdf("/media/amoki/8EB6E5E5B6E5CDB1/CHU/Nouveau\\ dossier/azerty.pdf", document, function(err, document) {
+      if(err) {
+        throw err;
+      }
+      document.should.have.property('datas');
+      document.should.have.property('document_type', "document");
+      document.datas.should.have.property('html');
+
+      fs.writeFileSync('/tmp/azerty.html', document.datas.html);
       done();
     });
   });
